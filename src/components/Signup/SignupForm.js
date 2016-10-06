c9791a3b753572cf2ff0f1675fd90c5a6b2949f4
Felipe García <felipe@toptierlabs.com>
2016-10-06 14:44:02 -0300

    Added validate.js for validation and refactored SignUp form

--- src/components/Signup/SignupForm.js
+++ src/components/Signup/SignupForm.js
@@ -1,31 +1,56 @@
 import React, { Component, PropTypes } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import FormField from './FormField';
+import {validate as validateForm} from 'validate.js';
+import * as constants from '../../constants';
+
+const errorMessages = {
+  email: 'Oops! this email is not valid',
+  username: 'You forgot to put your name!',
+  password: 'The password must be ' + constants.MIN_PASSWORD_LENGTH + ' characters long',
+  password_confirmation: 'Passwords don\'t match',
+}
+
+const signupDataConstraints = {
+  email: {
+    presence: { message: errorMessages.email },
+    email: { message: errorMessages.email },
+  },
+  username: {
+    presence: { message: errorMessages.username },
+  },
+  password: {
+    presence: { message: errorMessages.password },
+    length: {
+      minimum: constants.MIN_PASSWORD_LENGTH,
+      tooShort: errorMessages.password,
+    },
+  },
+  password_confirmation: {
+    presence: { message: errorMessages.password },
+    equality: {
+      attribute: 'password',
+      message: errorMessages.password_confirmation,
+    },
+  },
+}
+
+const validate = values => {
+  return validateForm(values, signupDataConstraints, {fullMessages: false});
+}
 
 class SignupForm extends Component {
   render() {
     const { handleSubmit, submitSignup } = this.props;
-
     return(
       <form
         onSubmit={handleSubmit(submitSignup)}
         className="form"
       >
-        <div>
-          <label htmlFor="email"> Email </label>
-          <Field name="email" component="input" type="email" />
-        </div>
-        <div>
-          <label htmlFor="username"> Username </label>
-          <Field name="username" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password"> Password </label>
-          <Field name="password" component="input" type="password" />
-        </div>
-        <div>
-          <label htmlFor="password_confirmation"> Password Confirmation </label>
-          <Field name="password_confirmation" component="input" type="password" />
-        </div>
+        <Field name="email" component={FormField} type="email" label="Email" />
+        <Field name="username" component={FormField} type="text" label="Username"/>
+        <Field name="password" component={FormField} type="password" label="Password" />
+        <Field name="password_confirmation" component={FormField} type="password" label="Password Confirmation" />
         <button type="submit">Submit</button>
       </form>
     );
@@ -38,7 +63,8 @@ SignupForm.propTypes = {
 }
 
 const SignupReduxForm = reduxForm({
-  form: 'signup'
+  form: 'signup',
+  validate
 })(SignupForm);
 
 export default SignupReduxForm;
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const AUTH_TOKEN_KEY = 'auth_token';
+export const MIN_PASSWORD_LENGTH = 8;
