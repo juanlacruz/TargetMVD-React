ea7e75a5c3e9a6619980f7dbfa58ac4ae410124d
Felipe García <felipe@toptierlabs.com>
2016-10-07 12:28:31 -0300

    Splitted webpack config into dev and prod files, added hot-loading for dev env

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,5 @@
 var config = {
-  entry: './src/index.jsx',
+  entry: './src/index.js',
   output: {
     path: 'public/',
     filename: 'bundle.js',
@@ -11,10 +11,13 @@ var config = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react', 'stage-2']
+          presets: ['es2015', 'react', 'stage-2'],
         },
       },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
     ],
   },
   devServer: {
